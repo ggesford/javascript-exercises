4c4a58109a909ddb257e9a8779c7b9e15de1c1dc
ggesford <gageg0411@gmail.com>
2024-12-13 01:12:42 -0800

    Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    const strings = []
+    for(let i = 0; i < num; i++) {
+        strings.push(string);
+    }
+    if(num < 0) {
+        return "ERROR";
+    } else {
+        return strings.join('');
+    }    
 };
-
 // Do not edit below this line
 module.exports = repeatString;
