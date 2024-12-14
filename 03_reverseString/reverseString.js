528973a44ee7a0f7cd95832cb5644ceb4d7509ed
ggesford <gageg0411@gmail.com>
2024-12-14 13:27:46 -0800

    Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const strings = [];
+    for(let i = string.length -1;i >= 0;i--) {
+        strings.push(string[i])
+    };
+    return strings.join('')
 };
 
 // Do not edit below this line
