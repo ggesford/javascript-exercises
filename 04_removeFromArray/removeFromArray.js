adc0e3cd4efee59981145a0c158182ab8c1a970e
ggesford <gageg0411@gmail.com>
2024-12-15 15:54:45 -0800

    incomplete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    const newArray = []
+    for(let i = 0;i <= array.length; i++) {
+            if(array[i] !== arg[j]) {
+                newArray.push(array[i]);
+            };
+    };
+    console.log(newArray);
+    return newArray;
+    
 };
 
 // Do not edit below this line
