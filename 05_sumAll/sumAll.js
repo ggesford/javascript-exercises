eb4fc96fcb2db7761908398d1019213027b0e3c7
ggesford <gageg0411@gmail.com>
2024-12-20 11:46:29 -0800

    complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,23 @@
 const sumAll = function(int1, int2) {
     let sum = 0;
+
     let smallInt, bigInt;
-    if(int1 < int2) {
+
+    if(int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)
+    ) {
+        return "ERROR";
+    } else if(int1 < int2) {
         smallInt = int1;
         bigInt = int2;
-    } else if (int1 || int2 < 0){
-        return "ERROR"
     } else {
         bigInt = int1;
         smallInt = int2;
-    }
+    };  
+
     for(let i = smallInt;i <= bigInt;i++) {
         sum += i;
-    }
+    };
+
     return sum;
 };
 
