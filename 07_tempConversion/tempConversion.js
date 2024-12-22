0844cf468cf0b22f12b2c3a34546fe7d2bf9ad12
ggesford <gageg0411@gmail.com>
2024-12-22 01:12:07 -0800

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
 //°F = (x - 32) * ⁠5/9⁠ °C
 const convertToCelsius = function(tempF) {
-
+  newTempC = (tempF - 32) * 5 / 9;
+  return Math.round(newTempC * 10) / 10;
 };
-//°C ≘ (x × ⁠  9 /  5  ⁠ + 32) °F
+//°C ≘ (x ×⁠ 9/5 - 32) °F
 const convertToFahrenheit = function(tempC) {
+  newTempF = (tempC * 9 / 5) + 32;
+  return Math.round(newTempF * 10) / 10;
 };
 
 // Do not edit below this line
