3ebfd5487ec688710704ea641af0f17df5f3f67f
ggesford <gageg0411@gmail.com>
2025-02-13 15:22:56 -0800

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,25 @@
-const add = function() {
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = arr => arr.reduce((acc, curr) => acc + curr, 0);
+
+const multiply = arr => arr.reduce((acc, curr) => acc * curr);
+
+const power = (num1, num2) => num1 ** num2;
+
+const factorial = function(num) {
+  const arr = [];
+  if (num === 0 || num === 1) {
+    return 1;
+  };
+  for(let i = num; i >= 2; i--) {
+    arr.push(i);
+  };
+  return arr.reduce((acc, curr) => acc * curr);
+};
 	
-};
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
