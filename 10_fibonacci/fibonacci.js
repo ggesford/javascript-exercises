fbec4dbb6d81eb9a8f1d6b6aa697b1af42ff65e3
ggesford <gageg0411@gmail.com>
2025-02-14 00:34:48 -0800

    complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) { 
+    if (Number(num) === 0) {
+        return 0;
+    } else if (Number(num) === 1) {
+        return 1;
+    } else if (Number(num) < 0) {
+        return "OOPS";
+    };   
+    const arr = [0,1];   
+    for(let i = 2;i <= Number(num); i++) {
+          arr.push(arr[i - 1] + arr[i - 2]);
+    }; 
+    return arr[Number(num)];  
 };
 
 // Do not edit below this line
