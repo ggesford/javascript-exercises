5c64f14942c14523971e073d7ee22dc9761a9dea
ggesford <gageg0411@gmail.com>
2025-02-14 01:36:47 -0800

    incomplete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,7 @@
-const findTheOldest = function() {
+const findTheOldest = arr => arr
+                                .map(person => person.age = person.yearOfDeath - person.yearOfBirth)
+                                .sort((a, b) => b.age - a.age)[0];
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
