de53c1230490e83210888e7472fc733fa26557a4
ggesford <gageg0411@gmail.com>
2025-02-13 16:09:10 -0800

    incompete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
-
+const palindromes = str => str
+                            .toLowerCase()
+                            .split('')
+                            .filter(elem => elem !== ',' && elem !== '!' && elem !== ' ')
+                            .join('') 
+                            === str
+                                .toLowerCase()
+                                .split('')  
+                                .filter(elem => elem !== ',' && elem !== '!' && elem !== ' ')
+                                .reverse()
+                                .join('');
 // Do not edit below this line
 module.exports = palindromes;
